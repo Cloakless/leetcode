b68385dbae5ff1aa66135959e26faa73d49603b6
Cloakless <tstone846@gmail.com>
2024-09-25 15:49:31 +0100

    Add 208.py

---
+++ code/208.py
@@ -0,0 +1,42 @@
+class Trie:
+
+    def __init__(self):
+        self.root = {}
+        
+
+    def insert(self, word: str) -> None:
+        curr = self.root
+        for c in word:
+            if c not in curr:
+                curr[c] = {}
+            curr = curr[c]
+        curr["is_word"] = True
+
+        
+
+    def search(self, word: str) -> bool:
+        curr = self.root
+        for c in word:
+            if c not in curr:
+                return False
+            curr = curr[c]
+        if "is_word" in curr:
+            return True
+        return False
+        
+
+    def startsWith(self, prefix: str) -> bool:
+        curr = self.root
+        for c in prefix:
+            if c not in curr:
+                return False
+            curr = curr[c]
+        return True
+        
+
+
+# Your Trie object will be instantiated and called as such:
+# obj = Trie()
+# obj.insert(word)
+# param_2 = obj.search(word)
+# param_3 = obj.startsWith(prefix)
