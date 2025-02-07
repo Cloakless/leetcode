20023289d5b51844f9f42cca46d46c887b67c99f
Cloakless <24829306+Cloakless@users.noreply.github.com>
2025-02-07 04:05:38 +0000

    Update 3160.py

--- code/3160.py
+++ code/3160.py
@@ -1,20 +1,24 @@
 class Solution:
     def queryResults(self, limit: int, queries: List[List[int]]) -> List[int]:
-        colours = defaultdict(int)
-        counter = defaultdict(int)
         ans = []
-        num_colours = 0
-        for x, y in queries:
-            if colours[x] != y:
-                # White -> colour
-                if counter[y] == 0:
-                    num_colours += 1
-                if colours[x] != 0:
-                    prev = colours[x]
-                    counter[prev] -= 1
-                    if counter[prev] == 0:
-                        num_colours -= 1
-                colours[x] = y
-                counter[y] += 1
-            ans.append(num_colours)
+        balls = defaultdict(int)
+        num_balls = defaultdict(int)
+        colours = 0
+        for ball, colour in queries:
+            if balls[ball] == 0:
+                # New colour
+                balls[ball] = colour
+                num_balls[colour] += 1
+                if num_balls[colour] == 1:
+                    colours += 1
+            else:
+                prev_colour = balls[ball]
+                num_balls[prev_colour] -= 1
+                if num_balls[prev_colour] == 0:
+                    colours -= 1
+                balls[ball] = colour
+                num_balls[colour] += 1
+                if num_balls[colour] == 1:
+                    colours += 1
+            ans.append(colours)
         return ans
