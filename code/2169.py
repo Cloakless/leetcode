2873d0a428685c77387b646cc3f195e849c6ae65
Cloakless <24829306+Cloakless@users.noreply.github.com>
2025-11-09 09:44:52 +0000

    Add 2169.py

---
+++ code/2169.py
@@ -0,0 +1,11 @@
+class Solution:
+    def countOperations(self, num1: int, num2: int) -> int:
+        ans = 0
+        while num1 * num2 != 0:
+            if num1 >= num2:
+                num1 -= num2
+            else:
+                num2 -= num1
+            ans += 1
+        return ans
+        
